46a68a44c3cbf7fe33b18f6940f22bdfa6b3e540
Daniel Duckworth <duckworthd@gmail.com>
2013-04-01 00:38:33 +0900

    Added HingeLoss function

--- yannopt/functions.py
+++ yannopt/functions.py
@@ -21,7 +21,7 @@ class Prox(object):
 class LogisticLoss(Function):
   """Logistic Regression loss function
 
-  min_{w} \sum_{i} log(1 + exp(-y_i x_i' w))
+    \sum_{i} log(1 + exp(-y_i x_i' w))
 
   Parameters
   ----------
@@ -57,6 +57,36 @@ class LogisticLoss(Function):
     for (i, y_pred) in enumerate(y_hat):
       result += y_pred * (1.0 - y_pred) * np.outer(X[i], X[i])
     return result
+
+
+class HingeLoss(Function):
+  """SVM's Hinge loss function
+
+    \sum_{i} max(0, 1 - y_i x_i' w)
+
+  Parameters
+  ----------
+  X : [n_samples, n_features] array-like
+      feature matrix
+  y : [n_samples] array-like
+      labels for each sample. Must be in {0, 1}
+  """
+  def __init__(self, X, y):
+    self.X = np.atleast_2d(X)
+    self.y = np.atleast_1d(y)
+
+  def eval(self, x):
+    X, y = self.X, self.y
+    y = 2 * y - 1
+    losses = np.maximum(0, 1 - y * X.dot(x))
+    return np.sum(losses)
+
+  def gradient(self, x):
+    # gradient[f](w) = \sum_{i} 1[1 - y_i x_i'w > 0] -1 * y_i x_i
+    X, y = self.X, self.y
+    y = 2 * y - 1
+    losses = np.maximum(0, 1 - y * X.dot(x))
+    return np.sum(((losses > 0) * y)[:, np.newaxis] * X, axis=0)
 
 
 class Quadratic(Prox, Function):
--- yannopt/tests/test_functions.py
+++ yannopt/tests/test_functions.py
@@ -15,9 +15,9 @@ def test_logistic_loss():
   # eval
   assert_allclose(
       f(w),
-      np.log(np.exp(X[0].dot(w))/(1 + np.exp(X[0].dot(w)))) + \
-      np.log(                1.0/(1 + np.exp(X[1].dot(w))))
-  )
+      -np.log(np.exp(X[0].dot(w))/(1 + np.exp(X[0].dot(w)))) + \
+      -np.log(                1.0/(1 + np.exp(X[1].dot(w))))
+  ), 'logistic loss'
 
   # gradient
   y_pred = np.array([
@@ -26,12 +26,36 @@ def test_logistic_loss():
   ])
   assert_allclose(
       f.gradient(w),
-      (y - y_pred)[0] * X[0] + (y - y_pred)[1] * X[1]
-  )
+      -(y - y_pred)[0] * X[0] + \
+      -(y - y_pred)[1] * X[1]
+  ), 'logistic loss gradient'
 
   # hessian
   assert_allclose(
       f.hessian(w),
       y_pred[0] * (1-y_pred[0]) * np.outer(X[0], X[0])
         + y_pred[1] * (1-y_pred[1]) * np.outer(X[1], X[1])
-  )
+  ), 'logistic loss hessian'
+
+
+def test_hinge_loss():
+  X = np.array([[ 1.0, 1.0],
+                [-1.0, 2.0]])
+  y = np.array([1, 1])
+
+  f = functions.HingeLoss(X, y)
+  w = np.array([2.0, -0.5])
+
+  # eval
+  assert_allclose(
+      f(w),
+      max(0, 1 - y[0] * X[0].dot(w)) + \
+      max(0, 1 - y[1] * X[1].dot(w))
+  ), 'hinge loss'
+
+  # gradient
+  assert_allclose(
+    f.gradient(w),
+    0 + \
+    - 1 * -1 * X[1]
+  ), 'hinge loss subgradient'
